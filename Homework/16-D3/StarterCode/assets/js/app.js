a731bc397150866ccb9e419b89f7ccc76e3bc819
Ruben D. Colmenares <rubendario11@gmail.com>
2019-07-05 17:58:33 -0500

    D3 Homework Update

--- Homework/16-D3/StarterCode/assets/js/app.js
+++ Homework/16-D3/StarterCode/assets/js/app.js
@@ -11,7 +11,7 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+
 var svg = d3.select(".chart")
   .append("svg")
   .attr("width", svgWidth)
@@ -21,13 +21,11 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-// Import Data
-d3.csv("../data/data.csv", function(err, healthData) {
+// Get Data
+d3.csv("assets/data/data.csv", function(err, datacsv) {
   if (err) throw err;
 
-  // Step 1: Parse Data/Cast as numbers
-   // ==============================
-  healthData.forEach(function(data) {
+   datacsv.forEach(function(data) {
     data.income = +data.income;
     data.age = +data.age;
     data.healthcare = +data.healthcare;
@@ -36,23 +34,17 @@ d3.csv("../data/data.csv", function(err, healthData) {
     data.poverty = +data.poverty;
   });
 
-  // Step 2: Create scale functions
-  // ==============================
   var xLinearScale = d3.scaleLinear()
-    .domain([d3.min(healthData, d => d.income - 5000), d3.max(healthData, d => d.income + 5000)])
+    .domain([d3.min(datacsv, d => d.income - 5000), d3.max(datacsv, d => d.income + 5000)])
     .range([0, width]);
 
   var yLinearScale = d3.scaleLinear()
-    .domain([d3.min(healthData, d=> d.obesity - 5), d3.max(healthData, d => d.obesity)])
+    .domain([d3.min(datacsv, d=> d.obesity - 5), d3.max(datacsv, d => d.obesity)])
     .range([height, 0]);
 
-  // Step 3: Create axis functions
-  // ==============================
   var bottomAxis = d3.axisBottom(xLinearScale);
   var leftAxis = d3.axisLeft(yLinearScale);
 
-  // Step 4: Append Axes to the chart
-  // ==============================
   chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
@@ -60,10 +52,9 @@ d3.csv("../data/data.csv", function(err, healthData) {
   chartGroup.append("g")
     .call(leftAxis);
 
-   // Step 5: Create Circles
-  // ==============================
+
   var circlesGroup = chartGroup.selectAll("circle")
-  .data(healthData)
+  .data(datacsv)
   .enter()
   .append("circle")
   .attr("cx", d => xLinearScale(d.income))
@@ -72,10 +63,10 @@ d3.csv("../data/data.csv", function(err, healthData) {
   .attr("fill", "lightblue")
   .attr("opacity", "1");
 
-    // Appending a label to each data point
+
     chartGroup.append("text")
     .selectAll("tspan")
-    .data(healthData)
+    .data(datacsv)
     .enter()
     .append("tspan")
         .attr("x", d => xLinearScale(d.income))
@@ -87,8 +78,7 @@ d3.csv("../data/data.csv", function(err, healthData) {
         .attr("fill", "white")
         .attr("font-size", 10, "bold");
 
-  // Step 6: Initialize tool tip
-  // ==============================
+
   var toolTip = d3.tip()
     .attr("class", "tooltip")
     .offset([80, -60])
@@ -96,21 +86,18 @@ d3.csv("../data/data.csv", function(err, healthData) {
       return (`${d.state}<br>Income (Median): $${d.income}<br>Obesity: ${d.obesity}%`);
     });
 
-  // Step 7: Create tooltip in the chart
-  // ==============================
+
   chartGroup.call(toolTip);
 
-  // Step 8: Create event listeners to display and hide the tooltip
-  // ==============================
+
   circlesGroup.on("click", function(data) {
     toolTip.show(data);
   })
-    // onmouseout event
+
     .on("mouseout", function(data, index) {
       toolTip.hide(data);
     });
 
-  // Create axes labels
   chartGroup.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 0 - margin.left + 40)
@@ -125,23 +112,17 @@ d3.csv("../data/data.csv", function(err, healthData) {
     .text("Houshold Income (Median)");
 
     // LABELS //
-    // Step 1: Append a div to the body to create tooltips, assign it a class
-  //= ======================================================
   var toolTip = d3.select("body").append("div")
     .attr("class", "tooltip");
 
-  // Step 2: Add an onmouseover event to display a tooltip
-  //= =======================================================
   circlesGroup.on("mouseover", function(d, i) {
     toolTip.style("display", "block");
     toolTip.html(`<strong>${d.state}</strong><br>Income: $${d.income}<br> Obesity: ${d.obesity}%`)
       .style("left", d3.event.pageX + "px")
       .style("top", d3.event.pageY + "px");
   })
-    // Step 3: Add an onmouseout event to make the tooltip invisible
+
     .on("mouseout", function() {
       toolTip.style("display", "none");
     });
- 
-  // END //
 });
